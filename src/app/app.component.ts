a7bdc76bea1a6a82cd5fc369ad209bf37faeeff0
Leandro Baroni <leandrobaroni2103@hotmail.com>
2024-04-07 17:42:23 -0300

    criação do big-card-component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,12 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {Component} from '@angular/core';
+import {RouterOutlet} from '@angular/router';
+import {HomePage} from "./pages/home/home.page";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HomePage],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
-  title = 'blog-leandro-baroni';
-}
+export class AppComponent {}
