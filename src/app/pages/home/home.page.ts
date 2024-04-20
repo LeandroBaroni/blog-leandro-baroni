dcdb2f6233768aad5ee56ddd943d09b7501d9fef
Leandro Baroni <leandrobaroni2103@hotmail.com>
2024-04-20 15:25:44 -0300

    criando o componente do menu-bar

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import {BigCardComponent} from "../../shared/components/big-card/big-card.component";
 import {SmallCardComponent} from "../../shared/components/small-card/small-card.component";
 import {MenuTitleComponent} from "../../shared/components/menu-title/menu-title.component";
+import {MenuBarComponent} from "../../shared/components/menu-bar/menu-bar.component";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [BigCardComponent, SmallCardComponent, MenuTitleComponent],
+  imports: [BigCardComponent, SmallCardComponent, MenuTitleComponent, MenuBarComponent],
   templateUrl: './home.page.html',
   styleUrl: './home.page.scss'
 })
